0bc7d65ba41f86831872f36193e481a77b7e5d1d
Kelvinng <you@example.com>
2025-01-05 17:03:37 +0800

    answered_2

--- q1.py
+++ q1.py
@@ -1,11 +1,10 @@
-
 # Modeling of the claw machine
-# fix the probability of getting the ice cream
-p = 1/6
-
+# fix the probability of getting the ice cream for the sake of profit 
+p = 1/7
+n = 10000
 import random
 data = []
-for i in range(100):
+for i in range(n):
     count = 0
     success = False
     while not success:
@@ -13,4 +12,10 @@ for i in range(100):
         success = random.random()<p
     data.append(count)
 
-print(data)
+#print(data)
+
+#sample probability
+prob = n/sum(data)
+#expected number of tries to get ice cream
+suc = 1/prob
+print(f'The number of tries before successfully getting the ice cream based on the data is {suc}')
